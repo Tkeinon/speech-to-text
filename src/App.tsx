6b1279a4b3aef3338dcd4018e114ed800ddc87f4
Tommi <tommi.keinonen@gmail.com>
2025-07-02 18:27:17 +0300

    feat: create useSpeechRecognition hook
    - With hook it's possible to start/stop listening device
    - Only browsers with SpeechRecognition API are supported

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import useSpeechRecognition from './hooks/useSpeechRecognition';
 
-const App: React.FC = () => {
-  return <div>Speech to Text App</div>;
-};
+function App() {
+    const {
+        isSupported,
+        startListening,
+        stopListening,
+        transcript,
+        listening,
+    } = useSpeechRecognition();
+
+    return (
+        <div>
+            {isSupported ? (
+                <>
+                    <button onClick={startListening} disabled={listening}>
+                        Start Listening
+                    </button>
+                    <button onClick={stopListening} disabled={!listening}>
+                        Stop Listening
+                    </button>
+                </>
+            ) : (
+                <p>Your browser does not support Speech Recognition.</p>
+            )}
+            <div>{transcript}</div>
+        </div>
+    );
+}
 
 export default App;
